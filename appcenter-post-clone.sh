7446f40854ef78adb15320b391648eea744a94cc
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-12 16:38:49 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,17 +1,15 @@
-APPLESIMUTILS_VERSION=0.5.22
-
-IS_IOS =    [ -z "$APPCENTER_XCODE_PROJECT" ];
-if [$IS_IOS]
-then     
-echo "Installing applesimutils...";
+#If an iOS app in AppCenter (APPCENTER_XCODE_PROJECT is AC env var)
+if ![ -n "$APPCENTER_XCODE_PROJECT" ]; then
+      APPLESIMUTILS_VERSION=0.5.22    
+      echo "Installing applesimutils..."
+      mkdir simutils
+      cd simutils
+      curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-${APPLESIMUTILS_VERSION}.tar.gz -o applesimutils.tar.gz
+      tar xzvf applesimutils.tar.gz
+      sh buildForBrew.sh .
+      cd ..
+      export PATH=$PATH:./simutils/build/Build/Products/Release
 fi
-#mkdir simutils
-#cd simutils
-#curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-${APPLESIMUTILS_VERSION}.tar.gz -o applesimutils.tar.gz
-#tar xzvf applesimutils.tar.gz
-#sh buildForBrew.sh .
-#cd ..
-#export PATH=$PATH:./simutils/build/Build/Products/Release
 
 echo "Installing correct node version..."
 export HOMEBREW_NO_AUTO_UPDATE=1
@@ -22,49 +20,50 @@ brew link node@8 --force --overwrite
 echo "Installing dependencies for detox tests..."
 npm install
 
-if [ -z "$APPCENTER_XCODE_PROJECT" ]; then 
- echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --update
+if [ -n "$APPCENTER_XCODE_PROJECT" ]; then 
+      echo "Building the iOS project for Detox tests..."
+      npx detox build --configuration ios.sim.release;
+      echo "Executing Detox tests for iOS..."
+      npx detox test --configuration ios.sim.release --loglevel verbose --cleanup
+else
+      #echo "Installing android system image..."
+      echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --update
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install tools
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install platform-tools
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install emulator
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'platforms;android-27'
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'build-tools;27.0.3'
+
+      #!! Run sdkmanager --list to see what's installed/available
       echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-27;google_apis;x86'
+
+      #the problem may be in google-apis image, default one may be useful.
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-25;default;x86_64'
+
+      #armeabi can be run w\o -no-accel flag
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-25;google_apis;armeabi-v7a'
 
-#echo "Installing android AVD..."
-#$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-24;google_apis;x86"
-#touch ~/.android/repositories.cfg
+      echo "Starting daemon..."
+      $ANDROID_HOME/platform-tools/adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill || true; done
 
-echo "Starting daemon..."
-$ANDROID_HOME/platform-tools/adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill || true; done
+      echo "Creating AVD..."
+      echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-27;google_apis;x86" --device "Nexus 5" --force
+      #echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-25;default;x86_64" --device "Nexus 5" --force
+      #echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-25;google_apis;armeabi-v7a" --force
 
-echo "Creating AVD..."
-#echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-25;default;x86_64" --device "Nexus 5" --force
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-27;google_apis;x86" --device "Nexus 5" --force
-#echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-28;google_apis;x86_64" --force
+      #do not delete
+      $ANDROID_HOME/tools/bin/avdmanager list avd
 
-
-
-$ANDROID_HOME/tools/bin/avdmanager list avd
-
-echo "Starting AVD..."
-# -qemu -no-window 
-nohup $ANDROID_HOME/emulator/emulator -avd emutest -no-snapshot -noaudio -no-boot-animm -no-accel & #> /dev/null 2>&1 &
-      $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+      echo "Starting AVD..."
+      #some flags to play with: -qemu -no-window -enable-kvm 
+      #https://stackoverflow.com/questions/47748948/android-emulator-never-finishes-booting-when-no-window-flag-is-used
       
+      nohup $ANDROID_HOME/emulator/emulator -avd emutest -no-snapshot -noaudio -no-boot-animm -no-accel & #> /dev/null 2>&1 & (uncomment to hide output)
+      #comment line after shell to start emulator async
+      $ANDROID_HOME/platform-tools/adb wait-for-device #shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82' 
       
-echo "Building the Android project for Detox tests..."
-npx detox build --configuration android.emu.debug 
-echo "Executing Detox tests for Android..."
-#!cp package.json android/app/build/outputs/apk/debug/package.json
-
-
-npx detox test -c android.emu.debug --loglevel verbose
-else
-      echo "Building the iOS project for Detox tests..."
-      npx detox build --configuration ios.sim.release;
-      echo "Executing Detox tests for iOS..."
-      npx detox test --configuration ios.sim.release --loglevel verbose --cleanup
+      echo "Building the Android project for Detox tests..."
+      npx detox build --configuration android.emu.debug 
+      echo "Executing Detox tests for Android..."
+      npx detox test -c android.emu.debug --loglevel verbose  
 fi
