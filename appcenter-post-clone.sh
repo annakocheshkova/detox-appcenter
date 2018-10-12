442256742a7b34f1b6f547960c557f3d0577e57f
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-12 14:05:13 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -24,7 +24,7 @@ echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n Nexus_5X_API_27_-_G
 $ANDROID_HOME/tools/bin/avdmanager list avd
 
 echo "Starting AVD..."
-nohup $ANDROID_HOME/emulator/emulator -avd Nexus_5X_API_27_-_GPlay  -no-snapshot & #> /dev/null 2>&1 &
+nohup $ANDROID_HOME/emulator/emulator -avd Nexus_5X_API_27_-_GPlay -no-accel -no-snapshot & #> /dev/null 2>&1 &
       $ANDROID_HOME/platform-tools/adb wait-for-device #shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
       
 echo "Installing applesimutils..."
