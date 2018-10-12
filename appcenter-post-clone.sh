9a096de6541dc67ffa70a1ed9e68758772f16c3b
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-12 18:02:11 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -35,7 +35,7 @@ else
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'build-tools;27.0.3'
 
       #!! Run sdkmanager --list to see what's installed/available
-      echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-24;google_apis;armeabi-v7a'
+      echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-23;google_apis;armeabi-v7a'
       
       #echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-27;google_apis;x86'
 
@@ -49,7 +49,7 @@ else
       $ANDROID_HOME/platform-tools/adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill || true; done
 
       echo "Creating AVD..."
-      echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n Nexus_5X_API_21_-_GPlay -k "system-images;android-24;google_apis;armeabi-v7a" --device "Nexus 5" --force
+      echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n Nexus_5X_API_21_-_GPlay -k "system-images;android-23;google_apis;armeabi-v7a" --device "Nexus 5" --force
       #echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-27;google_apis;x86" --device "Nexus 5" --force
       #echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-25;default;x86_64" --device "Nexus 5" --force
       #echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n emutest -k "system-images;android-25;google_apis;armeabi-v7a" --force
@@ -63,7 +63,7 @@ else
       
       nohup $ANDROID_HOME/emulator/emulator -avd Nexus_5X_API_21_-_GPlay -gpu on  -no-boot-anim -partition-size 2048 -wipe-data -skin "1080x1920" & #> /dev/null 2>&1 & (uncomment to hide output)
       #comment line after shell to start emulator async
-      $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; ' # input keyevent 82'
+      $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
       
       echo "Building the Android project for Detox tests..."
       npx detox build --configuration android.emu.debug 
