5e69baf65c07e6b718b7aaabd86bba05705d21d2
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-12 15:58:38 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -30,7 +30,7 @@ $ANDROID_HOME/tools/bin/avdmanager list avd
 
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --update
 echo "Starting AVD..."
-nohup $ANDROID_HOME/emulator/emulator -no-window -wipe-data -qemu -enable-kvm -snapshot -no-boot-anim -noaudio -avd emutest & #> /dev/null 2>&1 &
+nohup $ANDROID_HOME/emulator/emulator -avd emutest -no-window -wipe-data -qemu -enable-kvm -snapshot -no-boot-anim -noaudio & #> /dev/null 2>&1 &
       $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
       
 echo "Installing applesimutils..."
